fe2cebf0db7599a7bf050d702b048503d49a8f27
Ryume Taguchi <tgcrym01@gmail.com>
2022-11-04 16:35:13 +0900

    Implement health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use actix_web::{middleware::NormalizePath, web, App, HttpResponse, HttpServer};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind(("127.0.0.1", 8080)).unwrap();
+    HttpServer::new(move || {
+        App::new()
+            .wrap(NormalizePath::trim())
+            .route("/health_check", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run()
+    .await?;
+
+    Ok(())
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::NoContent().finish()
 }
