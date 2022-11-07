4a565d8e9407b41f5709d8462b36dd089cdb0175
Ryume Taguchi <tgcrym01@gmail.com>
2022-11-07 11:46:04 +0900

    create test modules (#9)

---
+++ tests/api/helpers.rs
@@ -0,0 +1,9 @@
+use twimi::startup::ApiServer;
+
+pub async fn spawn_app() -> String {
+    let server = ApiServer::build(("localhost", 0)).expect("Failed to build server as a background task");
+    let port = server.port();
+    let _ = tokio::spawn(server.run());
+
+    format!("http://localhost:{port}")
+}
