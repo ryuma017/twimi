e1a204c44014df5150c08460705da977d4486489
Ryume Taguchi <tgcrym01@gmail.com>
2022-11-19 23:36:50 +0900

    fix error response

--- twimi-app/src/server/middleware.rs
+++ twimi-app/src/server/middleware.rs
@@ -1,9 +1,12 @@
-use std::sync::Arc;
+use std::{
+    fmt::{Debug, Display},
+    sync::Arc,
+};
 
 use actix_web::{
     body::MessageBody,
     dev::{ServiceRequest, ServiceResponse},
-    error::{ErrorUnauthorized, InternalError},
+    error::InternalError,
     HttpMessage, HttpResponse,
 };
 use actix_web_lab::middleware::Next;
@@ -17,26 +20,35 @@ pub async fn reject_unauthenticated_user<B: MessageBody>(
     request: ServiceRequest,
     next: Next<B>,
 ) -> Result<ServiceResponse<B>, actix_web::Error> {
-    let Some(token) = request
+    let jwt = request
         .parts()
         .0 // &HttpRequest
         .headers()
         .get("Authorization")
-        .map(|v| v.to_str().unwrap().strip_prefix("Bearer ").unwrap()) else {
-            return Err(ErrorUnauthorized("No token provided."))
-        };
+        .ok_or_else(|| Unauthorized("The 'Authorization' header was missing."))?
+        .to_str()
+        .map_err(|_| Unauthorized("The 'Authorization' header was not a valid UTF8 string."))?
+        .strip_prefix("Bearer ")
+        .ok_or_else(|| Unauthorized("The authorization scheme was not `Bearer`."))?;
     let jwt_service: &dyn JwtService = request.app_data::<Arc<AppModule>>().unwrap().resolve_ref();
     let username: Username = jwt_service
-        .decode(token)
+        .decode(jwt)
         .map(|username| username.into())
-        .map_err(|e| {
-            InternalError::from_response(
-                e,
-                HttpResponse::Unauthorized()
-                    .insert_header(("WWW-Authenticate", r#"Bearer realm="jwtssoprotectedrealm""#))
-                    .finish(),
-            )
-        })?;
+        .map_err(Unauthorized)?;
     request.extensions_mut().insert(username);
     next.call(request).await
+}
+
+#[allow(non_snake_case)]
+fn Unauthorized<T>(cause: T) -> actix_web::Error
+where
+    T: Display + Debug + 'static,
+{
+    InternalError::from_response(
+        cause,
+        HttpResponse::Unauthorized()
+            .insert_header(("WWW-Authenticate", r#"Bearer realm="jwtssoprotectedrealm""#))
+            .finish(),
+    )
+    .into()
 }
